3fb6a9747caa9191b1d1306d4181d10aa058f268
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-10-01 09:28:45 -0400

    queue using two stacks: solved using two stacks

--- queue-using-two-stacks/program.py
+++ queue-using-two-stacks/program.py
@@ -9,24 +9,57 @@ Your class should support the enqueue and dequeue methods like a standard queue.
 
 class Queue:
     def __init__(self):
+        self.stack = []
         self.queue = []
 
+    def print_queue(self):
+        text = []
+        for i in range(len(self.queue)-1,-1,-1):
+            text.append(str(self.queue[i]))
+        print(f'Queue: {"->".join(text)}')
+
     def enqueue(self, val):
-        self.queue.append(val)
+        self.stack.append(val)
+        self.queue = []
+        for i in range(len(self.stack)-1, -1, -1):
+            self.queue.append(
+                self.stack[i]
+            )
+        print(f'Queued: {val}')
+        self.print_queue()
+        print()
 
     def dequeue(self):
-        front = self.queue[0]
-        self.queue = self.queue[1:]
-        return front
+        if not self.queue:
+            return "Empty"
+
+        popped = self.queue.pop()
+        self.stack = []
+        for i in range(len(self.queue)-1, -1, -1):
+            self.stack.append(
+                self.queue[i]
+            )
+        print(f'Dequeued: {str(popped)}')
+        self.print_queue()
+        print()
+
 
 def main():
     q = Queue()
-    q.enqueue(1)
-    q.enqueue(2)
     q.enqueue(3)
-    print(q.dequeue())
-    print(q.dequeue())
-    print(q.dequeue())
+    q.enqueue(5)
+    q.dequeue()
+    q.enqueue(1)
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
+    q.enqueue(4)
+    q.enqueue(2)
+    q.dequeue()
+    q.enqueue(4)
+    q.enqueue(7)
+    q.dequeue()
+    q.dequeue()
 
 if __name__ == '__main__':
     main()
